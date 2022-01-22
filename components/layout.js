0698ba894e55545f8fc11ca092b134bcee9d8b87
Michael Beckwith <tw2113@gmail.com>
2022-01-21 19:30:35 -0600

    current status

---
+++ components/layout.js
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+
+const name = 'Michael'
+export const siteTitle = 'Michael\'s Bookshelf'
+
+export default function Layout({children, home}) {
+	return (
+		<div className="w-10/12 mx-auto">
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+				<meta
+					name="description"
+					content="What has Michael been reading?"
+				/>
+			</Head>
+			<header className={styles.header}>
+				{home ? (
+					<>
+						<h1 className="text-3xl">{name}'s Books</h1>
+					</>
+				) : (
+					<></>
+				)}
+			</header>
+			<main>{children}</main>
+			{!home && (
+				<div className={styles.backToHome}>
+					<Link href="/">
+						<a>← Back to home</a>
+					</Link>
+				</div>
+			)}
+		</div>
+	)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,182 +1,53 @@
 import Head from 'next/head'
-import Link from 'next/link'
+import Image from 'next/image'
+import Layout, {siteTitle} from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Michael's Bookshelf</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+import {fetchBookData} from "../lib/posts";
 
-      <main>
-        <h1 className="title">
-          Books!
-        </h1>
+export async function getStaticProps() {
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-        </div>
-      </main>
+	const books = await fetchBookData();
 
-      <footer>
-        <a
-          href="https://apiratelifefor.me"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by reading
-        </a>
-      </footer>
+	return {
+		props: {
+			books
+		}
+	}
+}
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+export default function Home({ books }) {
+	let filteredbooks = books.filter(book => book._embedded['wp:featuredmedia'] !== undefined)
+	return (
+		<Layout home>
+			<Head>
+				<title>{siteTitle}</title>
+			</Head>
+			<section className="">
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+				<p>*page turn*</p>
+				<p>
+					I like to read. This is some of what I've read. Total: {filteredbooks.length}
+				</p>
+			</section>
 
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+			<article className="flex flex-wrap w-full">
+					{
+						filteredbooks.map(
+							(book) =>
+							<div className="flex-auto w-3/12">
+								<Image
+									src={book._embedded['wp:featuredmedia'][0].media_details.sizes.medium.source_url}
+									alt=""
+									layout="fixed"
+									width="225"
+									height="300"
+								/>
+							</div>
+						)
+					}
+			</article>
+		</Layout>
+	)
 }
