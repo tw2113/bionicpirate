4404e384a6c7d75a8f4820fa3a6197442bfaa67c
Michael Beckwith <tw2113@gmail.com>
2022-02-28 19:40:17 -0600

    make a bit more responsive, and add an alt text

--- components/book.js
+++ components/book.js
@@ -5,12 +5,12 @@ import Image from "next/image";
 export default function Book(props) {
 	const {book} = props
 	return (
-		<div key={book.id} className="flex-auto w-3/12">
+		<div key={book.id} className="flex-auto w-12/12 md:w-3/12">
 			<Link href={`https://www.indiebound.org/book/${encodeURIComponent(book.pbc_book_isbn[0])}`}>
 				<a target="_blank" rel="noopener noreferrer">
 					<Image
 						src={book._embedded['wp:featuredmedia'][0].media_details.sizes.medium.source_url}
-						alt=""
+						alt={book.title.rendered}
 						layout="fixed"
 						width="225"
 						height="300"
