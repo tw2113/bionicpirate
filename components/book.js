410c04bd896919730732ccc371d5029f088cc059
Michael Beckwith <tw2113@gmail.com>
2022-02-28 19:11:48 -0600

    create a new component

---
+++ components/book.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
+
+
+export default function Book(props) {
+	const {book} = props
+	return (
+		<div key={book.id} className="flex-auto w-3/12">
+			<Link href={`https://www.indiebound.org/book/${encodeURIComponent(book.pbc_book_isbn[0])}`}>
+				<a target="_blank" rel="noopener noreferrer">
+					<Image
+						src={book._embedded['wp:featuredmedia'][0].media_details.sizes.medium.source_url}
+						alt=""
+						layout="fixed"
+						width="225"
+						height="300"
+					/>
+				</a>
+			</Link>
+		</div>
+	)
+}
