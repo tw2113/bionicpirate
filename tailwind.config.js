f275866c5419f550d9ed1917dc2fdeaf2fa0006f
Michael Beckwith <tw2113@gmail.com>
2022-02-28 19:40:35 -0600

    get some screensizes created in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
 			scheeles_green: '#478800',
 			scheeles_blue: '#00478A',
 			scheeles_purple: '#8A0047'
+		},
+		screens: {
+			xs       : '480px',
+			sm       : '640px',
+			portrait : '768px',
+			md       : '960px',
+			md_max   : {max: '960px'},
+			landscape: '1024px',
+			lg       : '100%',
+			limit    : '1270px'
 		}
 	},
 	plugins: [],
