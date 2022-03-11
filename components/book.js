20bb5e895580ff1738c9d276efa989f2ecc0592b
Michael Beckwith <tw2113@gmail.com>
2022-03-10 18:47:26 -0600

    some decoding, use alt text from api, switch out indiebound link to individual book link

--- components/book.js
+++ components/book.js
@@ -1,23 +1,25 @@
 import Link from "next/link";
 import Image from "next/image";
-
+import he from 'he'
 
 export default function Book(props) {
 	const {book} = props
 	return (
 		<div key={book.id} className="flex-auto mb-8 w-12/12 md:w-3/12">
-			<Link href={`https://www.indiebound.org/book/${encodeURIComponent(book.pbc_book_isbn[0])}`}>
-				<a target="_blank" rel="noopener noreferrer">
+			<Link href={`/books/${book.slug}`}>
+				<a>
 					<Image
 						src={book._embedded['wp:featuredmedia'][0].media_details.sizes.medium.source_url}
-						alt={book.title.rendered}
+						alt={book._embedded['wp:featuredmedia'][0].alt_text}
 						layout="fixed"
 						width="225"
 						height="300"
 					/>
 				</a>
 			</Link>
-			<p>Total pages: {book.pbc_total_pages[0]}</p>
+			<p>{ he.decode( book.title.rendered ) }<br/>
+				Total pages: {book.pbc_total_pages[0]}
+			</p>
 		</div>
 	)
 }
