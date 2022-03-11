22e113542dc05fd84c61a120d0f88768406cffe9
Michael Beckwith <tw2113@gmail.com>
2022-03-10 22:19:22 -0600

    more styling

--- components/book.js
+++ components/book.js
@@ -5,7 +5,7 @@ import he from 'he'
 export default function Book(props) {
 	const {book} = props
 	return (
-		<div key={book.id} className="flex-auto mb-8 w-12/12 md:w-3/12">
+		<div key={book.id} className="flex-auto mb-8 md:mr-8 landscape:mr-0 w-12/12 sm:w-6/12 md:w-3/12 text-center">
 			<Link href={`/books/${book.slug}`}>
 				<a>
 					<Image
