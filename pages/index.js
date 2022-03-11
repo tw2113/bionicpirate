6468fc416ff860d65aadc422a5461b2e4dd2f72e
Michael Beckwith <tw2113@gmail.com>
2022-03-11 16:31:08 -0600

    import cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Layout, {siteTitle} from '../components/layout'
 import Book from '../components/book'
 import Link from 'next/link'
-import utilStyles from '../styles/utils.module.css'
 
 import {fetchBookChest} from "../lib/chest";
 
