2f3d99c24a2779a70f109e9d7d27bf91cc6675c1
Michael Beckwith <tw2113@gmail.com>
2022-03-11 19:10:00 -0600

    fetch our reading durations

--- components/meta.js
+++ components/meta.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
 
 export default function Meta(props) {
-	const {author, isbn, pages} = props.meta
+	const {author, isbn, pages, start, finished} = props.meta
 
 	return (
 		<div key="{isbn}">
 			Author: {author}<br/>
 			Pages: {pages}<br/>
+			Reading duration: {start} to {finished}<br/>
 			ISBN13: <Link href={`https://www.indiebound.org/book/${isbn}`}>
 				<a target="_blank" rel="noopener noreferrer">{isbn}</a>
 			</Link>
