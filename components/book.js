a3bf6845584098536aaa47f824ae38d1889025c9
Michael Beckwith <tw2113@gmail.com>
2024-02-09 18:51:37 -0600

    update and simplify book component with more destructuring

--- components/book.js
+++ components/book.js
@@ -3,19 +3,20 @@ import Image from "next/image";
 import he from 'he'
 
 export default function Book(props) {
-	const {book} = props
+	const {book} = props;
+	const [mediumBook] = book._embedded['wp:featuredmedia'];
+	const { media_details: { sizes: { medium } } } = mediumBook;
+
 	return (
 		<div key={book.id} className="flex-auto mb-8 md:mr-8 landscape:mr-0 w-12/12 sm:w-6/12 md:w-3/12 text-center">
 			<Link href={`/books/${book.slug}`}>
-				<a>
-					<Image
-						src={book._embedded['wp:featuredmedia'][0].media_details.sizes.medium.source_url}
-						alt={book._embedded['wp:featuredmedia'][0].alt_text}
-						layout="fixed"
-						width="225"
-						height="300"
-					/>
-				</a>
+				<Image
+					src={medium.source_url}
+					alt={mediumBook.alt_text}
+					layout="fixed"
+					width={medium.width}
+					height={medium.height}
+				/>
 			</Link>
 			<p>{ he.decode( book.title.rendered ) }</p>
 		</div>
