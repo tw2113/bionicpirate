189d6c9dc26f42516575804f48c4df4c3c5eae58
Michael Beckwith <tw2113@gmail.com>
2024-04-26 10:08:19 -0500

    extend, not replace color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,6 @@
 module.exports = {
 	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}",],
 	theme: {
-		colors: {
-			transparent: 'transparent',
-			current: 'currentColor',
-			scheeles_green: '#478800',
-			scheeles_blue: '#00478A',
-			scheeles_purple: '#8A0047',
-			mummy_brown: '#8f4a21'
-		},
 		screens: {
 			xs       : '480px',
 			sm       : '640px',
@@ -22,6 +14,14 @@ module.exports = {
 		extend: {
 			boxShadow: {
 				scheeles_boxshadow: 'box-shadow: 0 0 50px 25px var(--scheeles_purple)'
+			},
+			colors: {
+				transparent: 'transparent',
+				current: 'currentColor',
+				scheeles_green: '#478800',
+				scheeles_blue: '#00478A',
+				scheeles_purple: '#8A0047',
+				mummy_brown: '#8f4a21'
 			}
 		}
 	},
