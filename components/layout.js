3b34d23ec5a0d95d08be552e97bfd62a6ad4905c
Michael Beckwith <tw2113@gmail.com>
2024-04-26 09:28:52 -0500

    semicolons and a new current year variable display

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
-const name = 'Michael'
-export const siteTitle = 'Bionic Pirate'
+const name = 'Michael';
+export const siteTitle = 'Bionic Pirate';
 
 export default function Layout({children, home}) {
+
+	const curYear = new Date().getFullYear();
 	return (
 		<div className="w-10/12 mx-auto">
 			<Head>
@@ -30,7 +32,7 @@ export default function Layout({children, home}) {
 			</header>
 			<main className="bookshelf">{children}</main>
 			<footer className="mb-8">
-				<small>&copy;2022 Most all photos self taken. All copyrights respected.</small>
+				<small>&copy;2022-{curYear} Most all photos self taken. All copyrights respected.</small>
 			</footer>
 		</div>
 	)
